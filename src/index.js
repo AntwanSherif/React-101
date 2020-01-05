849d7f99ed1e89a328cc1ffca0d4e4b113fa96c8
AntwanSherif <antwansherif@gmail.com>
2020-01-05 18:00:03 +0200

    Create first component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-const element = <h1>Hello World!</h1>;
+const Welcome = () => <h1>Hello World!</h1>;
+
+const element = <Welcome />;
 // console.log(element);
 
 ReactDOM.render(element, document.getElementById('root'));
